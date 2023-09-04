d665f9e24b10c8d64ee74cbfdb17292d7729c7a8
Sammurih <141797001+Sammurih@users.noreply.github.com>
2023-09-04 04:57:09 +0000

    punto 1.4

--- Taller_1/indices_refraccion.py
+++ Taller_1/indices_refraccion.py
@@ -10,10 +10,14 @@ def tuplasdata (archivo):
     while centinela == False:
         linea=datos.readline()
         if "data: |" in linea:
-            while linea != "":
+            while linea != "" and centinela == False:
                 linea=datos.readline().strip()
-                if linea != "":
-                    lista=linea.split(" ")
+                lista=linea.split(" ")
+                try:
+                    float(lista[0])
+                except ValueError:
+                    centinela = True
+                if linea != "" and centinela == False:
                     tupla=(lista[0],lista[1])
                     listaarreglo.append(tupla)
             centinela = True
@@ -21,3 +25,19 @@ def tuplasdata (archivo):
     datos.close()
     return arreglo
 print(tuplasdata("Taller_1/Vidrio/BF1.yml"))
+
+X= tuplasdata("Taller_1/Plásticos Comerciales/Kapton.yml")
+print(X)
+def ejesgraf (arraytuplas):
+    x=[]
+    y=[]
+    for i in arraytuplas:
+        x.append(i[0])
+        y.append(i[1])
+    ejex=(np.array(x))
+    ejey=(np.array(y))
+    plt.scatter(ejex,ejey)
+    plt.show()
+    return None
+
+print(ejesgraf(X))
