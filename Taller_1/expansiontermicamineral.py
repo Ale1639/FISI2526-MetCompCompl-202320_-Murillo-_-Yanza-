5c5d350192210b971fc95bc498d93c5cef13f9fd
Ale1639 <a.yanza@uniandes.edu.co>
2023-09-07 23:39:44 -0500

    3

--- Taller_1/expansiontermicamineral.py
+++ Taller_1/expansiontermicamineral.py
@@ -9,18 +9,45 @@ class ExpansionTermicaMineral (Mineral):
         linea= pd.read_csv(archivo,",")
         self.temperatura=np.array(linea["celsius_temperature"])
         self.volumen= np.arrray(linea["volume_cc"])
-
-    def coeficiente_dila(self):
+    
+    def coeficiente_exp(self):
         def diff_cen(yi,yf,xi,xf):
             return (yf-yi)/(2*(xf-xi))
-        ytemp=np.zeros(len(self.temperaturas),2)
-        x=np.zeros(len(self.temperaturas),2)
-
+        ytemp=np.zeros(len(self.temperatura),2)
+        x=np.zeros(len(self.temperatura),2)
+        sum=0
         for temp in range(len(self.temperatura)):
             ytemp[temp][0]=self.temperatura[temp]
             x[temp][0]=self.temperatura[temp]
 
         for cont in range(len(self.temperatura)):
-            if cont==0:
-                
+            if cont==(len(self.temperatura)-1):
+                ytemp[cont][1]=diff_cen(self.temperatura[cont-1],self.temperatura[cont],self.volumen[cont-1],self.volumen[cont])
+                x[cont][1]=(1/self.volumen[cont])*ytemp[cont][1]
+            elif cont==0:
+                ytemp[cont][1]=diff_cen(self.temperatura[cont],self.temperatura[cont+1],self.volumen[cont],self.volumen[cont+1])
+                x[cont][1]=(1/self.volumen[cont])*ytemp[cont][1]
+            else:
+                ytemp[cont][1]=diff_cen(self.temperatura[cont-1],self.temperatura[cont+1],self.volumen[cont-1],self.volumen[cont+1])
+                x[cont][1]=(1/self.volumen[cont])*ytemp[cont][1]
+        for i in range (len(self.temperatura)):
+            sum+=x[i][1]
+
+            error=np.std(x[:][1])
+            promedio=sum/len(self.temperatura)
+            figure=plt.figure(figsize=(10,4))
+            abajo=figure.add_subplot(121)
+            arriba=figure.add_subplot(122)
+            abajo.scatter(np.array(self.temperatura),self.volumen)
+            print(x[:,1])
+            arriba.scatter(np.array(self.temperatura),x[:,1])
+
+            return np.array([figure, promedio, error])
+
+        
+
+
+
+
+
         
