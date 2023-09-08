2c40ea83b2904bb78bd5b45e42cefb2a9d37a487
Ale1639 <a.yanza@uniandes.edu.co>
2023-09-07 23:56:00 -0500

    k

--- Taller_1/expansiontermica_olivina_grafito.py
+++ Taller_1/expansiontermica_olivina_grafito.py
@@ -2,6 +2,5 @@ import numpy as np
 import matplotlib.pyplot as plt
 from expansiontermicamineral import ExpansionTermicaMineral as ex
 
-print(Exp
-    (self ,r"Taller_1 - Parte 2\graphite_mceligot_2016.csv"))
+print(ex(r"Taller_1 - Parte 2\graphite_mceligot_2016.csv"))
 #print(ex.coeficiente_exp(r"Taller_1 - Parte 2\olivine_angel_2017.csv"))
